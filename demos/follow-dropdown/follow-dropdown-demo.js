23e3d3a674626f286da20ffa753c1ac140d6f1e8
Ross Pfahler <ross.pfahler@gmail.com>
2014-08-05 20:45:15 -0700

    Add follow dropdown

---
+++ demos/follow-dropdown/follow-dropdown-demo.js
@@ -0,0 +1,31 @@
+var createAuthButton = require('auth/contrib/auth-button');
+var FollowButton = require('collection-feed/follow-button');
+var auth = require('livefyre-auth');
+var authButton = createAuthButton(auth, document.getElementById('auth-button'));
+var delegate = window.delegate = auth.createDelegate('http://www.livefyre.com');
+// auth.delegate(delegate);
+var token = 'eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9.eyJkb21haW4iOiAiZGVtby5meXJlLmNvIiwgImV4cGlyZXMiOiAxNDA5Mjc4ODg4LjgyMDM0OSwgInVzZXJfaWQiOiAic3lzdGVtIn0.EqskRFnL_Ewp41veS8mMBNlGWQYcQ1MW-x9cqVrHcPM';
+auth.authenticate({
+    livefyre: token
+});
+
+var dropdownEl = document.getElementById('rosstest');
+var Dropdown = require('collection-feed/follow-dropdown');
+
+var drop = new Dropdown({
+  followButton: function (tag) {
+    return new FollowButton(tag);
+  },
+  tags: [{
+      "id": "urn:livefyre:livefyre.com:site=286470:topic=123",
+      "displayName": "Topic 123",
+      "objectType": "topic"
+  },{
+      "id": "urn:livefyre:livefyre.com:topic=ABC",
+      "displayName": "Topic ABC - Network",
+      "objectType": "topic"
+  }]
+});
+
+drop.setElement(dropdownEl);
+drop.render();
---
+++ src/follow-dropdown.js
@@ -0,0 +1,101 @@
+module.exports = Dropdown;
+
+var inherits = require('inherits');
+var packageAttribute = require('./package-attribute');
+var Button = require('streamhub-ui/button');
+var Command = require('streamhub-ui/command');
+var Popover = require('streamhub-ui/popover');
+var View = require('view');
+var $ = require('jquery');
+
+/**
+ * Dropdown encapulates a clickable element and a popover.
+ * @param {function} opts.followButton
+ * @param {Object.<string, string>} opts.tags
+ */
+function Dropdown(opts) {
+    View.apply(this, arguments);
+    this._followBtns = [];
+    this._button = this._createButton();
+    this._popover = null;
+}
+inherits(Dropdown, View);
+
+Dropdown.prototype._createButton = function () {
+    var cmd = new Command(function () {
+        if (!this._popover) {
+            this._popover = this._createPopover();
+        }
+    }.bind(this));
+
+    return new Button(cmd, {
+        className: 'drop-btn',
+        label: 'Follow <span class="caret"></span>'
+    });
+};
+
+Dropdown.prototype._createPopover = function () {
+    var self = this;
+    var popover = new Popover({
+        parentEl: this.el
+    });
+    var $el = $('<ul />');
+    this._followBtns = this.opts.tags.map(function (tag) {
+        var li = $('<li>' + tag.displayName + '</li>');
+        var btn = this.opts.followButton(tag);
+        btn.setElement(li[0]);
+        btn.render();
+        $el.append(li);
+        return btn;
+    }.bind(this));
+
+    popover._position = 'bottom';
+    popover.render();
+    popover.setContentNode($el[0]);
+    popover.resizeAndReposition(this._button.el);
+    popover.show(this._button.$el[0]);
+
+    setTimeout(function () {
+        $('body').on('click', hideShare);
+    }, 100);
+
+    function hideShare(ev) {
+        if ($(ev.target).closest(popover.el).length) {
+            return;
+        }
+        popover.destroy();
+        self._popover = popover = null;
+        $('body').off('click', hideShare);
+    }
+
+    return popover;
+};
+
+Dropdown.prototype.elClass = 'lf-drop';
+
+/** @override */
+Dropdown.prototype.setElement = function () {
+    if (this.el) {
+        packageAttribute.undecorate(this.el);
+    }
+    var ret = View.prototype.setElement.apply(this, arguments);
+    packageAttribute.decorate(this.el);
+    return ret;
+};
+
+/** @override */
+Dropdown.prototype.render = function() {
+    View.prototype.render.call(this);
+    this._button.render();
+    this.$el.append(this._button.$el);
+};
+
+/** @override */
+Dropdown.prototype.destroy = function () {
+    View.prototype.destroy.call(this);
+    this._followBtns.forEach(function (btn) {
+        btn.destroy();
+    });
+    this._popover && this._popover.destroy();
+    this._button.destroy();
+};
